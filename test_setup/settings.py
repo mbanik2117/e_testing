01ed57a365e118bf94634a08c5b2978925c029c4
mbanik2117 <153079956+mbanik2117@users.noreply.github.com>
2024-04-20 16:22:47 +0530

    Update settings.py

--- test_setup/settings.py
+++ test_setup/settings.py
@@ -138,6 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
+# Celery configuration
+CELERY_BROKER_URL = 'redis:/redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
